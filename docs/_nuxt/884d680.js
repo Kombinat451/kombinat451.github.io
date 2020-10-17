71ef6d0122f11944c5e0dd3c7d90b8b0dbfba0cc
Kombinat451 Web <www@kombinat451.org>
2020-10-17 12:28:01 +0200

    Tabs - fix duplicated entries

---
+++ docs/_nuxt/884d680.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{235:function(t,e,l){"use strict";l.r(e);var n={props:{label:{type:String,required:!0}}},r=l(46),component=Object(r.a)(n,(function(){var t=this.$createElement;return(this._self._c||t)("b-tab",{staticClass:"m-2",attrs:{title:this.label}},[this._t("default")],2)}),[],!1,null,null,null);e.default=component.exports}}]);
