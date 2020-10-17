7f3f9fb330a0bf3ec6642169929472657f11ddd3
Kombinat451 Web <www@kombinat451.org>
2020-10-17 14:07:18 +0200

    component - add AssetImage
    Till now there is no image support for nuxt content. Fix by adding an
    AssetImage component

---
+++ components/global/AssetImage.vue
@@ -0,0 +1,29 @@
+<template>
+<p>
+  <b-img style="max-width:35em" :src="imgSrc" :alt="alt" center />
+</p>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    imgSrc() {
+      try {
+        return require(`~/assets/${this.src}`)
+      } catch (error) {
+        return null
+      }
+    }
+  }
+}
+</script>
