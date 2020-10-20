2d129deb1a5269461f347693b36860133ed844f9
Kohl Listi <kohllisti@riseup.net>
2020-10-20 22:26:35 +0200

    Fix viewport meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   head: {
     meta: [
       { charset: 'utf-8' },
-      { viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
     ]
   },
   router: {
