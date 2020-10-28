a207ad0a4156c3c5bb8600bfc71f132b630e4c63
Kombinat451 Web <www@kombinat451.org>
2020-10-28 12:11:01 +0100

    meta tags - open graph+twitter

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,8 +4,26 @@ export default {
   components: true,
   head: {
     meta: [
+      // Encoding
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
+
+      // Responsive / Bootstrap
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
+
+      // Open Graph
+      { property: 'og:title',       content: 'Kombinat451 - tech activist collective'},
+      { property: 'og:description', content: 'HTTP 451 Unavailable For Legal Reasons'},
+      { property: 'og:url',         content: 'https://www.kombinat451.org/'},
+      { property: 'og:type',        content: 'website'},
+      { property: 'og:image',       content: 'https://avatars3.githubusercontent.com/u/25828950'},
+
+      // Twitter
+      { name: 'twitter:card',        content: 'summary'},
+      { name: 'twitter:title',       content: 'Kombinat451 - tech activist collective'},
+      { name: 'twitter:description', content: 'HTTP 451 Unavailable For Legal Reasons'},
+      { name: 'twitter:image',       content: 'https://avatars3.githubusercontent.com/u/25828950'},
+      { name: 'twitter:site',        content: '@Kombinat451'},
+      { name: 'twitter:creator',     content: '@Kombinat451'}
     ]
   },
   router: {
